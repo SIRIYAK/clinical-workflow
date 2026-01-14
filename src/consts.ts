6715d8340f47c8f46ee6369b403330bf38d51be1
Siriyak cr <crs@eikontx.com>
2026-01-14 21:44:15 +0530

    tested ok

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Siriyak CR - Clinical Automation';
-export const SITE_DESCRIPTION = 'Professional portfolio and insights on clinical trial data automation.';
